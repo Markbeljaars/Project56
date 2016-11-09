4d35fe4876758ce16135c844d2e52e105293ce86
minde cheng <mindecheng@live.nl>
2016-11-09 11:08:11 +0100

    request and update admin fix(address + email)

---
+++ Project56/WebApplication2/WebApplication2/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.DAL;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        private AccountContext ac = new AccountContext();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string test()
+        {
+            return "test ";
+        }
+
+        
+        public ActionResult table()
+        {
+            return View();
+        }
+    }
+}
---
+++ Project56/WebApplication2/WebApplication2/DAL/AccountContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace WebApplication2.DAL
+{
+    public class AccountContext
+    {
+
+
+        public AccountContext()
+        {
+
+        }
+
+
+    }
+}
